08ba8875ba87e43973205ea37dd0505ef5e167b4
Leonov <leonov.ua97@gmail.com>
2021-10-18 12:23:25 +0300

    Remove excess item from localstorage

--- src/common/services/getWeather.js
+++ src/common/services/getWeather.js
@@ -6,12 +6,16 @@ import { setFalse } from '../../store/features/loadingSlice'
 import { refreshInput } from '../../store/features/inputSlice'
 import { setSavedFalse } from '../../store/features/savedSlice'
 import { setErrorFalse, setErrorTrue } from '../../store/features/cityApiSlice'
+import { increment } from '../../store/features/counterSlice'
 // import { GetHoursTemp } from './getHoursTemp'
 
 export const GetWeather = () => {
   const city = store.getState().cityApi.value
   const apiKey = 'bfda20ca597bc0f4138fa15d276de911'
   const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+  localStorage.removeItem('__agwt_rt')
+  store.dispatch(increment())
 
   axios.get(URL)
     .then(res => {
